0ba4bbc084c5db08251795eb66dc189a19f65d6b
Niclas Olmenius <niclas@voysys.se>
2022-04-21 15:36:50 +0200

    add linux build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+cd thirdparty
+bash ./build.sh
+cd ..
+
+INSTALL_DIR=${PWD}/install
+
+rm -rf build/
+rm -rf artifacts/
+
+mkdir build
+mkdir install
+
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" ..
+cmake --build . --config Release
+cmake --install .
+cd ..
